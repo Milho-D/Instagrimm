cf68a1895301ea0cf4cee7a864fa5a562eed881b
Milho <labulledemilho@gmail.com>
2019-06-28 16:59:40 +0200

    US02-Post-encours

---
+++ TP5/TP5/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP5.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public DateTime dateCreated { get; set; }
+        public DateTime dateUpdated { get; set; }
+
+        public User UserPost {get;set;}
+    }
+}
---
+++ TP5/TP5/Services/PostService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TP5.Models;
+
+namespace TP5.Services
+{
+    public class PostService
+    {
+        private PostService() { }
+
+        private PostService _Instance;
+        public PostService Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new PostService();
+                }
+
+                return _Instance;
+            }
+        }
+
+        private Post _CurrentPost;
+
+        public Post GetCurrentPost()
+        {
+            return _CurrentPost;
+        }
+    }
+}
